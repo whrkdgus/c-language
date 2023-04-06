9aa39cc454a9a5114f948dc6897527de9e142d4b
11 11 <qwqwqw13134@gmail.com>
2023-04-06 05:11:42 +0000

    49.c

---
+++ 49.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int num1 = 30;
+	float num2 = 3.14;
+	char munja = 'm';
+	char munjas[5]="ansan";
+	printf("%d\n",num1);
+	printf("%f\n",num2);
+	printf("%c\n",munja);
+	printf("%s\n",munjas);
+	return 0;
+}
