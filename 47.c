12bdcfdf61e3033210c4c8d5107b5779bad1293a
11 11 <qwqwqw13134@gmail.com>
2023-04-06 05:11:12 +0000

    47.c

---
+++ 47.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int num = 39;
+	int num2 = 93;
+	int num3 = 33;
+	printf("%d", num);
+	printf("%d", num2);
+	printf("%d", num3);
+	printf("%d", num4);
+	return 0:
+}
