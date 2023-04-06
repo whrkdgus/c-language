260ba771b9c57c44a51f1752419dc84a4b7181d9
11 11 <qwqwqw13134@gmail.com>
2023-04-06 05:11:58 +0000

    52.c

---
+++ 52.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	char a[5] = {'K','O','R','E','A'};
+	printf("%c\n", a[1]);
+	printf("%s\n",a);
+	char b[6] = {'K','O','R','E','A', '\0'};
+	printf("%s\n", b);
+	char c[6] = "KOREA";
+	printf("%s\n",c);
+	return 0;
+}
