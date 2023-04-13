6632163f1d585cbddbbc2e52039812904976e18a
11 11 <qwqwqw13134@gmail.com>
2023-04-13 06:24:34 +0000

    1

---
+++ src/54.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+	int a, a1, a2; //
+	int A, cat, Cat, cAt, caT;
+	int _a, __a, ___;
+	int if;
+	
+	return 0;
+}
---
+++ src/55.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int a =3; //변수선언 및 초기화
+	a = a + 6;
+	if(a>5) printf("%d\n", a);
+	//printf("%d\n),a -6;
+	
+	return 0;
+}
